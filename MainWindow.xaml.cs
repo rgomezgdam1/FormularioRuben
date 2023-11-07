683b4c063d56e9aa9f3bcc60e15dd7edc8ab3d08
rgomezgdam1 <rgomezgdam1@gmail.com>
2023-11-07 08:11:30 +0100

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace FormularioEmpleadoRubenGomez
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AbrirArchivo_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos de Imagen|*.jpg;*.jpeg;*.png;*.gif;*.bmp;*.ico|Todos los archivos (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string rutaArchivo = openFileDialog.FileName;
+                // Aquí puedes usar la variable 'rutaArchivo' para trabajar con el archivo seleccionado.
+                BitmapImage bitmap = new BitmapImage(new Uri(rutaArchivo));
+                imagenSalida.Source = bitmap;
+            }
+            
+        }
+    }
+}
